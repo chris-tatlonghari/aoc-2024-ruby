573f453c4f7723089679253e587f7f79e2f03959
chris.tatlonghari <christat98@gmail.com>
2025-10-19 18:35:27 -0400

    Solve day 6 part 2

--- solutions/day6.rb
+++ solutions/day6.rb
@@ -7,6 +7,7 @@ class Day6 < Solution
   DIRECTION_STEPS = [[0,-1],[1, 0],[0, 1],[-1, 0]]
   OBSTACLE_CHAR = '#'
   GUARD_CHAR = '^'
+  EMPTY_SPACE = '.'
 
   def self.title = "Day 6: Guard Gallivant"
 
@@ -37,7 +38,38 @@ class Day6 < Solution
   end
 
   def self.solve2
-    puts :unknown
+    loops_detected = 0
+    0.upto(MATRIX.size-1) do |r|
+      0.upto(MATRIX.first.size-1) do |c|
+        next unless MATRIX[r][c] == EMPTY_SPACE
+
+        obstacles_hit = Set.new
+        x, y = guard_coordinates
+        direction = 0 # up = 0, right = 1, down = 2, left = 3
+        MATRIX[r][c] = OBSTACLE_CHAR
+
+        while (0..MATRIX.first.size-1).cover?(x) && (0..MATRIX.size-1).cover?(y)
+          dx, dy = DIRECTION_STEPS[direction]
+          break unless (0..MATRIX.first.size-1).cover?(x + dx) && (0..MATRIX.size-1).cover?(y + dy)
+
+          if MATRIX[y + dy][x + dx] == OBSTACLE_CHAR
+            direction = (direction + 1) % DIRECTION_STEPS.size
+            if obstacles_hit.include?([[y + dy],[x + dx],[direction]])
+              loops_detected += 1
+              break
+            end
+            obstacles_hit.add([[y + dy],[x + dx],[direction]])
+          else
+            x += dx
+            y += dy
+          end
+        end
+
+        MATRIX[r][c] = EMPTY_SPACE
+      end
+    end
+
+    puts loops_detected
   end
 
   private
